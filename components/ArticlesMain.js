bde41d3998643bb94b17af859bb7477c22c4b8de
joelmasters <joelmasters@gmail.com>
2022-01-28 14:19:14 -0800

    added display none for default video controls

---
+++ components/ArticlesMain.js
@@ -0,0 +1,10 @@
+import styles from './ArticlesMain.module.css';
+
+export default function ArticlesMain() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Physics of Disc Flight</h1>
+      <h5>Coming soon</h5>
+    </div>
+  )
+}
